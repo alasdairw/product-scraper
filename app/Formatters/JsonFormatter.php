cb5f139684099303b44f8e32fc417b0b345b1240
Alasdair Watson <alasdair@alasdair.biz>
2016-08-29 11:05:25 +0000

    clean up JsonFormatter Test

--- app/Formatters/JsonFormatter.php
+++ app/Formatters/JsonFormatter.php
@@ -2,14 +2,24 @@
 namespace App\Formatters;
 interface Formatter
 {
-    public function format(array $array);    
+    public function getFormattedOutput();
 }
 
 class JsonFormatter implements Formatter
 {
-    public function format(array $array)
+    public function __construct(\Parser $parser)
+    {
+        $this->parser = $parser;
+    }
+
+    private function format(array $array)
     {
         return json_encode($array,true);
+    }
+
+    public function getFormattedOutput()
+    {
+        return json_encode($this->parser->getParsedData(),true);
     }
 
 }
--- tests/JsonFormatterTest.php
+++ tests/JsonFormatterTest.php
@@ -1,15 +1,25 @@
 <?php 
 use PHPUnit\Framework\TestCase;
 use App\Formatters\JsonFormatter;
+use App\Parsers\IndexParser;
 
-class JSONParserTest extends TestCase
+class JSONFormatterTest extends TestCase
 {
-    public function testFormat()
+    public function setUp()
     {
-        $array = array('foo' => 'bar');
-        $formatter = new JsonFormatter();
-        $output = $formatter->format($array);
+        $parser = Mockery::mock('\Parser');
+        $parser->shouldReceive('getParsedData')->once()->andReturn(array('results' => [array('title'=>'Some title')],'total'=>'12.00'));
+        $this->formatter = new JsonFormatter($parser);
+    }
+
+    public function testGetFormattedOutput()
+    {
+        $output = $this->formatter->getFormattedOutput();
+        
         $this->assertJson($output);
         $data = json_decode($output,true);
+        $this->assertArrayHasKey('results',$data);
+        $this->assertArrayHasKey('total',$data);
+        $this->assertArrayHasKey('title',$data['results'][0]);
     }
 }
